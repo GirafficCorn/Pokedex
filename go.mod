6dc4adac0ad618b31af5c560743ce8d23e7792b3
Brandon Tallman <bgtall96@gmail.com>
2025-05-02 20:05:36 -0700

    wrote clean input and test for this function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GirafficCorn/Pokedex
+
+go 1.24.2
---
+++ repl_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "This Is Another Test",
+			expected: []string{"this", "is", "another", "test"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Length of actual and expected do not match! Expected: %v Actual: %v", c.expected, actual)
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Words do not match!")
+			}
+		}
+	}
+
+}
